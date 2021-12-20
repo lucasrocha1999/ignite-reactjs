874c6f50caca21ae936680b77361271da7633556
Lucas Lira <lucasrocha446@gmail.com>
2021-12-19 22:05:27 -0300

    feat: add first components

---
+++ 01-github-explorer/src/components/RepositoryList/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import "../../styles/repositories.scss";
+import { RepositoryItem } from "../RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
+
+export function RepositoryList() {
+  // useEffect(() => {
+  //   fetch("https://api.github.com/orgs/rocketseat/repos")
+  //     .then((response) => response.json())
+  //     .then((data) => setRepositories(data));
+  // }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}

1478d26764cc2ef66c2a0d3fe9d25d4594deae51
Lucas Lira <lucasrocha446@gmail.com>
2022-01-05 08:15:17 -0300

    feat: add mock with miragejs

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.urlPrefix = 'http://localhost:3000'
+    
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Freelancer",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00"),
+        },
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>

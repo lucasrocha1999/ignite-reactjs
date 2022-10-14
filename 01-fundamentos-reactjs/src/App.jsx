0d30255cb172a76e71fc8318d5e5f2773b90bdd2
Lucas Lira <lucasrocha446@gmail.com>
2022-10-13 23:23:57 -0300

    add component sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,31 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './Post'
+
+import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
   return (
-    <>
-    <Header />
-      <Post 
-        author="Lucas Lira"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-      />
+    <div>
+      <Header />
 
-      <Post 
-        author="Nathy"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-      />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Lucas Lira"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
+
+          <Post 
+            author="Nathy"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+          />
+        </main>
+      </div>
+    </div>
   )
 }

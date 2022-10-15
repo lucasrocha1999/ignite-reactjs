40fa96913f8bfd9cf627ad334c04841fed3789b6
Lucas Lira <lucasrocha446@gmail.com>
2022-10-15 10:34:41 -0300

    chore: finish sidebar style

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/42046570?v=4" />
+
                 <strong>Lucas Lira</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>

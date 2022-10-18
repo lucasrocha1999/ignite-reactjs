7f8ed6fada860f44e61e192aae2289b76d20d83e
Lucas Lira <lucasrocha446@gmail.com>
2022-10-18 12:12:42 -0300

    add component avatar

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 export function Post() {
@@ -6,7 +8,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/42046570?v=4" />
+                    <Avatar src="https://github.com/lucasrocha1999.png" />
                     <div className={styles.authorInfo}>
                         <strong>Lucas Lira</strong>
                         <span>Dev Front-End</span>
--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/42046570?v=4" />
+                <Avatar src="https://github.com/lucasrocha1999.png" />
 
                 <strong>Lucas Lira</strong>
                 <span>Web Developer</span>

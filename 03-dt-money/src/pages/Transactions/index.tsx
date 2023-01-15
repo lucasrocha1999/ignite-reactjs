d75e4021abcdf6e4a8e426f1816eda460c7668db
Lucas Lira <lucasrocha446@gmail.com>
2023-01-15 00:43:15 -0300

    feat: add header component

---
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+
+// import { Container } from './styles';
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}

9fed1c1880caa3e20de010409803aa61108048c1
Lucas Lira <lucasrocha446@gmail.com>
2023-05-13 20:38:25 -0300

    feat: add Header component

--- 03-desafio-github-blog/src/App.tsx
+++ 03-desafio-github-blog/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/default";
 import { GlobalStyle } from "./styles/global";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <Home />
     </ThemeProvider>
   );
 }
---
+++ 03-desafio-github-blog/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+
+import * as S from './styles'
+
+export function Home() {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  )
+}
